f2a5088268d5a9cab4d1dd358b2bfc77c32192e5
Roman <sviridoroman@gmail.com>
2021-12-02 01:49:14 +0300

    app router fix

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
 
-import swaggerUI from 'swagger-ui-express';
-import path from 'path';
-import YAML from 'yamljs';
-
 import directorRouter from './resources/directors/director.router';
 import filmRouter from './resources/films/film.router';
 import resultRouter from './resources/results/result.router';
@@ -11,11 +7,7 @@ import resultRouter from './resources/results/result.router';
 import { notFound, successHttpLogger, errorHttpLogger, errorLogger } from './middlewares';
 
 const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
 app.use(express.json());
-
-app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   if (req.originalUrl === '/') {

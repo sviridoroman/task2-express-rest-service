41f94aa5c34b1d6480621e49b813b820700fecc8
Roman <sviridoroman@gmail.com>
2021-12-02 01:09:56 +0300

    add logging services

--- src/middlewares/errorLogger.ts
+++ src/middlewares/errorLogger.ts
@@ -5,14 +5,13 @@ import AppError from '../classes/appError.class';
 import logger from '../common/logger';
 
 export const errorLogger = (error: Error, _req: Request, res: Response, next: NextFunction) => {
-    
   if (error instanceof AppError) {
     const statusCode = error.statusCode || 500;
     const status = error.status || 'ERROR';
     const code = error.code || 'SERVER_ERROR';
 
     const message = error.message || 'Error';
-    
+
     res.errorMessage = message;
     logger.error(`${status} ${error.message}`);
     res.status(statusCode).json({ status, code, message });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import config from './common/config';
 import app from './app';
-import { uncaughtExceptionHandler, unhandledRejectionHandler } from './common/errorHandler';
 
+import { uncaughtExceptionHandler, unhandledRejectionHandler } from './common/errorHandler';
+import config from './common/config';
 
 const { PORT } = config;
 
 const server = app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+
 process.on('uncaughtException', uncaughtExceptionHandler(server, true));
 process.on('unhandledRejection', unhandledRejectionHandler(server, false));
